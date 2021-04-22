66b407660c9b62445f1927b1cdab6f19c2125527
Daniel Drexler <aeturnum@gmail.com>
2021-04-22 14:46:51 -0700

    Change functions removed and depricated by OTP 24

--- src/esaml_binding.erl
+++ src/esaml_binding.erl
@@ -57,14 +57,15 @@ decode_response(_, SAMLResponse) ->
 encode_http_redirect(IdpTarget, SignedXml, Username, RelayState) ->
   Type = xml_payload_type(SignedXml),
   Req = lists:flatten(xmerl:export([SignedXml], xmerl_xml)),
-  Param = http_uri:encode(base64:encode_to_string(zlib:zip(Req))),
-  RelayStateEsc = http_uri:encode(binary_to_list(RelayState)),
   FirstParamDelimiter = case lists:member($?, IdpTarget) of true -> "&"; false -> "?" end,
-  Username_Part = redirect_username_part(Username),
-  iolist_to_binary([IdpTarget, FirstParamDelimiter, "SAMLEncoding=", ?deflate, "&", Type, "=", Param, "&RelayState=", RelayStateEsc | Username_Part]).
+  QueryString = uri_string:compose_query([
+      {"SAMLEncoding", ?deflate}, 
+      {Type, base64:encode_to_string(zlib:zip(Req))},
+      {RelayState, RelayState}
+      | redirect_username_part(Username) ]),
+  iolist_to_binary([IdpTarget, FirstParamDelimiter, QueryString]).
 
-redirect_username_part(Username) when is_binary(Username), size(Username) > 0 ->
-  ["&username=", http_uri:encode(binary_to_list(Username))];
+redirect_username_part(Username) when is_binary(Username), size(Username) > 0 -> [{"username", Username}];
 redirect_username_part(_Other) -> [].
 
 %% @doc Encode a SAMLRequest (or SAMLResponse) as an HTTP-POST binding
